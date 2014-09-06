1b71f43a9ddd8ea46ed0042b2ddeb39963f98c37
Ryan Strug <ryan@intercyph.com>
2014-09-06 18:40:14 -0400

    Initial Commit

---
+++ LakeCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LakeCalculator
+//
+//  Created by Ryan Strug on 9/6/14.
+//  Copyright (c) 2014 Ryan Strug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
